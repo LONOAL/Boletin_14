979c034e83e8861f2e51e411cac0b227a9f96dc0
LONOAL <lnogueirapan@danielcastelao.org>
2022-11-25 09:29:49 +0100

    Readme y JavaDoc añadido

--- src/com/lorenzo/Boletin_14/Main.java
+++ src/com/lorenzo/Boletin_14/Main.java
@@ -4,6 +4,13 @@ import com.lorenzo.Partes.Cpu;
 import com.lorenzo.Partes.Monitor;
 import com.lorenzo.Partes.Teclado;
 
+/**
+ * Boletin_14: Main.
+ *
+ * @author Lorenzo Nogueira
+ * @version 25.11.2022
+ */
+
 public class Main {
     public static void main(String[] args) {
         Monitor mo = new Monitor(27);
--- src/com/lorenzo/Partes/Teclado.java
+++ src/com/lorenzo/Partes/Teclado.java
@@ -1,18 +1,45 @@
 package com.lorenzo.Partes;
 
+    /**
+    * Boletin_14: Da valor a la marca del teclado.
+    *
+    * @author Lorenzo Nogueira
+    * @version 25.11.2022
+    */
+
 public class Teclado {
     private String marca;
 
+    /**
+     * Constructor vacio.
+     */
+
     public Teclado() {
     }
+
+    /**
+     * Constructor.
+     * @param marca: marca.
+     */
 
     public Teclado(String marca) {
         this.marca = marca;
     }
 
+    /**
+     * Getter.
+     * @return marca: marca.
+     */
+
     public String getMarca() {
         return marca;
     }
+
+    /**
+     * Setter.
+     * @param marca
+     * Dar valor marca.
+     */
 
     public void setMarca(String marca) {
         this.marca = marca;
